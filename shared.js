730fc8ecd1521bd4efebff2fb24515d93ceb66a8
mollamba <132110552+mollamba@users.noreply.github.com>
2026-09-21 22:55:30 -0400

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,8 @@
+document.body.insertAdjacentHTML('afterbegin', `
+  <nav class="nav">
+    <a href="/">home</a>
+    <a href="/resume/">resume</a>
+    <a href="/projects/">projects</a>
+    <a href="/about/">about</a>
+  </nav>
+`);
